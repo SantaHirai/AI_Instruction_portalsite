d47cb1ccd1f03b066fd0a6fcb49e2f3366269877
SantaHirai <pockyou15@gmail.com>
2025-12-17 11:02:21 +0900

    キーワードにGoogle検索を追加した

--- src/data/keywords.ts
+++ src/data/keywords.ts
@@ -8,5 +8,9 @@ export const KEYWORDS: KeywordItem[] = [
     {
         term: 'AI',
         description: '人工知能の略称。'
+    },
+    {
+        term: 'Google検索',
+        description: 'インターネット上で調べたいキーワードを入力すると、それに関連するウェブサイトや情報を探し出して一覧で表示してくれるサービス'
     }
 ];
